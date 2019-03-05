a2fec399022795d395f241c6247c43e72bd301d5
Ryan <ruifafan@gmail.com>
2019-03-05 20:39:51 +0800

    添加JedisPool配置

--- src/com/fanruifa/Test.java
+++ src/com/fanruifa/Test.java
@@ -1,13 +1,20 @@
 package com.fanruifa;
 
-import com.fanruifa.redis.RedisUtil;
+import com.fanruifa.redis.JedisPoolUtil;
 
 import redis.clients.jedis.Jedis;
 
 public class Test {
 	public static void main(String[] args) {
-		Jedis jedis = RedisUtil.Jedis("127.0.0.1", 6379);
-		jedis.set("name", "1234567");
-		System.out.println("name:::" + jedis.get("name"));
+		Jedis jedis = null;
+		try {
+			jedis = JedisPoolUtil.getJedisPool().getResource();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (jedis != null) {
+			jedis.set("name", "123456723");
+			System.out.println("name:::" + jedis.get("name"));
+		}
 	}
 }
---
+++ src/com/fanruifa/redis/JedisPoolUtil.java
@@ -0,0 +1,47 @@
+package com.fanruifa.redis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * 单机版Redis
+ * 
+ * @author Ryan
+ *
+ */
+public class JedisPoolUtil {
+
+	private static JedisPool jedisPool;
+
+	public static JedisPool getJedisPool() throws IOException {
+		if (jedisPool == null) {
+			InputStream inputStream = JedisPoolUtil.class.getClassLoader().getResourceAsStream("redis-config.properties");
+			System.out.println(inputStream);
+			Properties properties = new Properties();
+			properties.load(inputStream);
+			JedisPoolConfig config = new JedisPoolConfig();
+			int maxActive = Integer.valueOf(properties.getProperty("maxActive"));
+			int maxIdle = Integer.valueOf(properties.getProperty("maxIdle"));
+			long maxWait = Long.valueOf(properties.getProperty("maxWait"));
+			boolean testOnBorrow = Boolean.valueOf(properties.getProperty("testOnBorrow"));
+			String host = properties.getProperty("host");
+			int port = Integer.valueOf(properties.getProperty("port"));
+			int timeout = Integer.valueOf(properties.getProperty("timeout"));
+			config.setMaxActive(maxActive);
+			config.setMaxIdle(maxIdle);
+			config.setMaxWait(maxWait);
+			config.setTestOnBorrow(testOnBorrow);
+			jedisPool = new JedisPool(config, host, port, timeout);
+		}
+		return jedisPool;
+	}
+
+	public static Jedis getJedis() {
+		return jedisPool == null ? null : jedisPool.getResource();
+	}
+}
